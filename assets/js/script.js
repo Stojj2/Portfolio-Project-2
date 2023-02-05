14e34d8f25b928db6eeb7e7124e4cf71613239f5
Stojj2 <stojj2@hotmail.com>
2023-02-05 19:32:28 +0000

    Add to functions in script.js file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,48 @@
+document.addEventListener("DOMContentLoaded", setup);
 let guessCount = 0;
 let gameWord = "";
 
-let buttons = document.getElementsByTagName("button");
 
-for (let button of buttons) {
-    button.addEventListener("click", function () {
-        if (this.getAttribute("data-type") === "planets") {
-            startGame("planets")
-        } else if (this.getAttribute("data-type") === "cars") {
-            startGame("cars")
-        } else if (this.getAttribute("data-type") === "submit") {
+/**
+ * This function will be caled when the DOM content is loaded
+ * and run the setup steps for the game
+ */
+function setup() {
+    document.getElementById("answer-box").disabled = true;
+    document.getElementById("button-submit").disabled = true;
+    document.getElementById("word-field").style.backgroundColor = "#ffffff9e";
+    document.getElementById("hint-header").textContent = "WELCOME!";
+    
+    let buttons = document.getElementsByTagName("button");
+    document.getElementById("answer-box").addEventListener("keydown", function (event) {
+        if (event.key === "Enter") {
             checkAnswer();
         }
-    });
+    })
+
+    for (let button of buttons) {
+        button.addEventListener("click", function () {
+            if (this.getAttribute("data-type") === "planets") {
+                startGame("planets")
+            } else if (this.getAttribute("data-type") === "cars") {
+                startGame("cars")
+            } else if (this.getAttribute("data-type") === "submit") {
+                checkAnswer();
+            }
+        });
+    }
 }
 
 /**
  * The start game function prepare the game area with game data.
  */
-document.getElementById("answer-box").addEventListener("keydown", function (event) {
-    if (event.key === "Enter") {
-        checkAnswer();
-    }
-})
-
-/**
- * The start game function prepare the game area with game data.
- */
 function startGame(category) {
+    document.getElementById("guesses-field").style.backgroundColor = "";
+    document.getElementById("hint-field").style.backgroundColor = "";
+    document.getElementById("hint-header").textContent = "Hint:";
+    document.getElementById("answer-box").disabled = false;
+    document.getElementById("button-submit").disabled = false;
+    document.getElementById("answer-box").select();
     guessCount = 3;
     let guesses = document.getElementById("guesses-field");
     guesses.textContent = guessCount;
@@ -81,14 +96,22 @@ function checkAnswer() {
     console.log(guessedWord);
     if (guessedWord.toLowerCase() === gameWord.toLowerCase() && guessedWord !="") {
         let hint = document.getElementById("hint-field");
+
+        document.getElementById("hint-header").textContent = "CONGRATULATION!";
         hint.textContent = "You guessed right!";
+        document.getElementById("answer-box").disabled = true;
+        document.getElementById("button-submit").disabled = true;
+        document.getElementById("hint-field").style.backgroundColor = "#63b23dc7";
         let wordToBeGuessed = document.getElementById("word-field");
         wordToBeGuessed.textContent = gameWord;
 
-    } else if (guessCount === 0) {
-        endGame();
-    } else {
+    }  else {
+        document.getElementById("guesses-field").style.backgroundColor = "#c52f2f94";
         decreaseGuessCount();
+    }
+
+    if (guessCount === 0) {
+        endGame();
     }
     clearInput()
 }
@@ -111,6 +134,9 @@ function randomIndex() {
 }
 
 function endGame() {
+    document.getElementById("hint-header").textContent = "SORRY!";
+    document.getElementById("answer-box").disabled = true;
+    document.getElementById("button-submit").disabled = true;
     let guesses = document.getElementById("guesses-field");
     guesses.textContent = 0;
     let hint = document.getElementById("hint-field");
