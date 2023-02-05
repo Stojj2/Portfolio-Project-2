1dba7c5cdbcd5482f1f2dc778f581d3e4575c7b8
Stojj2 <stojj2@hotmail.com>
2023-02-05 11:14:12 +0000

    Add a second javaScript file named words.js witch will hold all the words and facts for the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,107 +1,3 @@
-let cars = [
-    {
-        'fact': "Inventor of the seatbelt",
-        "word": "volvo"
-    },
-
-    {
-        'fact': "The company is located in Germany",
-        "word": "bmw"
-    },
-
-    {
-        'fact': "Owned by the same family since 1899",
-        "word": "fiat"
-    },
-
-    {
-        'fact': 'In Latin means this car maker means "Listen"',
-        "word": "audi"
-    },
-
-    {
-        'fact': "The name of an inventor",
-        "word": "tesla"
-    },
-
-    {
-        'fact': "The founder of this company has the brand name in his last name",
-        "word": "ford"
-    },
-
-    {
-        'fact': " One of Indias biggest company",
-        "word": "tata"
-    },
-
-    {
-        'fact': "This company actually started out as a textiles business",
-        "word": "toyota"
-    },
-
-    {
-        'fact': "Swedish company that went bankrupt in 2011",
-        "word": "saab"
-    },
-
-    {
-        'fact': "The first diesel automobile",
-        "word": "mercedes"
-    }
-]
-
-let planets = [
-    {
-        'fact': "This planet is the biggest in our solar system",
-        "word": "jupiter"
-    },
-
-    {
-        'fact': "This planet is far away and counted as a dworf planet",
-        "word": "pluto"
-    },
-
-    {
-        'fact': "Closest planet to The Sun",
-        "word": "mercury"
-    },
-
-    {
-        'fact': "1 day on this planet is almost 1 Earth year",
-        "word": "venus"
-    },
-
-    {
-        'fact': "Surface is mainly water",
-        "word": "earth"
-    },
-
-    {
-        'fact': "Known as the Red Planet",
-        "word": "mars"
-    },
-
-    {
-        'fact': "Final planet in the Solar System",
-        "word": "neptune"
-    },
-
-    {
-        'fact': "Was classified as a planet for half a century",
-        "word": "ceres"
-    },
-
-    {
-        'fact': "Adorned with thousands of beautiful ringlets",
-        "word": "saturn"
-    },
-
-    {
-        'fact': "The coldest planet in the Solar System",
-        "word": "uranus"
-    }
-]
-
 let guessCount = 0;
 let gameWord = "";
 
@@ -167,14 +63,14 @@ function getWord(choosen) {
  * Selects a random object from planets array
  */
 function selectPlanetObject() {
-    return planets[randomIndex()];
+    return PLANETS[randomIndex()];
 }
 
 /**
  * Selects a random object from car array 
  */
 function selectCarObject() {
-    return cars[randomIndex()];
+    return CARS[randomIndex()];
 }
 
 /**
---
+++ assets/js/words.js
@@ -0,0 +1,103 @@
+const CARS = [
+    {
+        'fact': "Inventor of the seatbelt",
+        "word": "volvo"
+    },
+
+    {
+        'fact': "The company is located in Germany",
+        "word": "bmw"
+    },
+
+    {
+        'fact': "Owned by the same family since 1899",
+        "word": "fiat"
+    },
+
+    {
+        'fact': 'In Latin means this car maker means "Listen"',
+        "word": "audi"
+    },
+
+    {
+        'fact': "The name of an inventor",
+        "word": "tesla"
+    },
+
+    {
+        'fact': "The founder of this company has the brand name in his last name",
+        "word": "ford"
+    },
+
+    {
+        'fact': " One of Indias biggest company",
+        "word": "tata"
+    },
+
+    {
+        'fact': "This company actually started out as a textiles business",
+        "word": "toyota"
+    },
+
+    {
+        'fact': "Swedish company that went bankrupt in 2011",
+        "word": "saab"
+    },
+
+    {
+        'fact': "The first diesel automobile",
+        "word": "mercedes"
+    }
+]
+
+const PLANETS = [
+    {
+        'fact': "This planet is the biggest in our solar system",
+        "word": "jupiter"
+    },
+
+    {
+        'fact': "This planet is far away and counted as a dworf planet",
+        "word": "pluto"
+    },
+
+    {
+        'fact': "Closest planet to The Sun",
+        "word": "mercury"
+    },
+
+    {
+        'fact': "1 day on this planet is almost 1 Earth year",
+        "word": "venus"
+    },
+
+    {
+        'fact': "Surface is mainly water",
+        "word": "earth"
+    },
+
+    {
+        'fact': "Known as the Red Planet",
+        "word": "mars"
+    },
+
+    {
+        'fact': "Final planet in the Solar System",
+        "word": "neptune"
+    },
+
+    {
+        'fact': "Was classified as a planet for half a century",
+        "word": "ceres"
+    },
+
+    {
+        'fact': "Adorned with thousands of beautiful ringlets",
+        "word": "saturn"
+    },
+
+    {
+        'fact': "The coldest planet in the Solar System",
+        "word": "uranus"
+    }
+]
